058a36da9ac4bce59bb9704ac20cec5051f10287
cbravose <cbravose@uoc.edu>
2024-11-23 11:36:22 +0100

    cambios page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 import { database } from './firebaseConfig';
 
 export default function Home() {
-
+ 
   const [jugadores, setJugadores]: any = useState<any[]>([]);
 
   const loadData = () => {
@@ -23,29 +23,27 @@ export default function Home() {
           setJugadores(jugadoresArray);
         }
       });
-      /*
-      onValue(jugadoresRef, (snapshot) => {
-        const data = snapshot.val();
-        console.log(data);
-        if (data) {
-          const keys = Object.keys(data);
-          console.log(keys);
-          // Convierte el objeto en un array
-          const jugadoresArray = Object.keys(data).map((key) => ({
-            id: key, // Agrega el ID como parte del objeto
-            ...data[key],
-          }));
-          setJugadores(jugadoresArray);
-        }
-      });
-      */
-  };
-  loadData();
+      
+  }; 
+  useEffect(() => {
+    loadData();
+  }, []); 
+  
+ 
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {jugadores.map((jugador: any) => (
-          <li>{jugador.nombre}</li> // Usamos "key" para identificar cada elemento.
-        ))}
+      {jugadores.length > 0 ? (
+        jugadores.map((jugador:any) => (
+          <ul key={jugador.id}>
+            <li>
+              {jugador.nombre} {jugador.posicion} {jugador.edad} {jugador.partidos} {jugador.sexo} {jugadores.video} 
+            </li>
+          </ul>
+        ))
+      ) : (
+        <p>No hay jugadores disponibles.</p>
+      )}
     </div>
   );
 }
