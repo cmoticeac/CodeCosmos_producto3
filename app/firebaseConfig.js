cd06644660adbc0eec03161ff628889d8d6eca53
Liliana <ldiazib@uoc.edu>
2024-11-30 19:32:24 +0100

    codigo en expo con conexión a la bd

---
+++ app/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD1CfHJSBh8EhrA1J8gIRCDCfr4IrutpFk",
+  authDomain: "testapp-7b75d.firebaseapp.com",
+  databaseURL: "https://testapp-7b75d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "testapp-7b75d",
+  storageBucket: "testapp-7b75d.firebasestorage.app",
+  messagingSenderId: "196096250345",
+  appId: "1:196096250345:web:3f54252ed77c92ae7bfb78"
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
